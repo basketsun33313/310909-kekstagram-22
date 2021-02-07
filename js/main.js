55fb135565730866bd5ac6b200cae48e835a141f
basketsun33313 <basketsun33313@gmail.com>
2021-02-07 18:58:30 +0300

    firsts Function

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+let str = prompt("Введите строоку " , "");
+let maxlegth = +prompt("Введите максимальную длинну " , "")
+if(str == undefined || str == ""){
+    alert("Строка не введена");
+}else{
+    truncate();
+}
+function truncate() {
+    if (str.length >= maxlegth) {
+        return alert(str.slice(0 , maxlegth-1) + "...");
+    }else{
+        return alert(false);
+    }
+}
