abc0baad6774a74af7698e89a124917cebf10ab8
basketsun33313 <basketsun33313@gmail.com>
2021-03-02 10:29:49 +0300

    corrected the comments

--- js/main.js
+++ js/main.js
@@ -10,28 +10,24 @@ const SENTENSES = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const checkLength = (str, maxLength) => {
-  return str.length < maxLength;
-};
-
 const generatePictures = (count) => {
   const pictures = [];
-    for (let i = 0; i < count; i++) {
-      pictures.push({
-        id : i + 1,
-        url : 'photos/' + (i + 1) + '.jpg',
-        description : 'Описание',
-        likes: random(MIN_LIKES, MAX_LIKES),
-        comments: generateComments(random(0, MAX_COMMENTS))
-      });
-    }
+  for (let i = 0; i < count; i++) {
+    pictures.push({
+      id : i + 1,
+      url : 'photos/' + (i + 1) + '.jpg',
+      description : 'Описание',
+      likes: random(MIN_LIKES, MAX_LIKES),
+      comments: generateComments(random(0, MAX_COMMENTS)),
+    });
+  }
     return pictures;
 };
 
@@ -43,7 +39,7 @@ const generateComments = (count) => {
       id: i + 1,
       avatar: 'img/avatar-' + random(0, AVATAR_COUNT) + '.svg',
       name: NAMES[random(0, NAMES.length - 1)],
-      message: getRandomItems(SENTENSES, random(1, 2)).join(' ')
+      message: getRandomItems(SENTENSES, random(1, 2)).join(' '),
     });
   }
 
