f3ea992d78f96dae3af114939dbd81f4b1a83c64
basketsun33313 <basketsun33313@gmail.com>
2021-03-16 20:02:09 +0300

    fixed comments part 3

--- js/full-photo.js
+++ js/full-photo.js
@@ -4,36 +4,47 @@ let pictureLikes = pictureElement.querySelector('.likes-count');
 let pictureCountComments = pictureElement.querySelector('.comments-count');
 let pictureDesctiption = pictureElement.querySelector('.social__caption');
 let pictureBoxComments = pictureElement.querySelector('.social__comments');
-let tamplateFragment = pictureElement.querySelector('.social__comment').content;
+let tamplateFragment = pictureElement.querySelector('.social__comment');
 let buttonClose = pictureElement.querySelector('.big-picture__cancel');
 let counterCommentBox = pictureElement.querySelector('.social__comment-count');
 let commentLoader = document.querySelector('.comments-loader');
-let bodyBackground = document.querySelector('body');
+
+const renderComments = (comments) => {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < comments.length; i++) {
+    let commentElement = document.createElement('li');
+    commentElement.classList.add('social__comment');
+    let imageElement = document.createElement('img');
+    imageElement.classList.add('social__picture');
+    imageElement.src = comments[i].avatar;
+    imageElement.alt = comments[i].name;
+    imageElement.width = 35;
+    imageElement.height = 35;
+    let socialTextElement = document.createElement('p');
+    socialTextElement.textContent = comments[i].message;
+    commentElement.appendChild(imageElement);
+    commentElement.appendChild(socialTextElement);
+    fragment.appendChild(commentElement);
+  }
+  pictureBoxComments.innerHTML = '';
+  pictureBoxComments.appendChild(fragment);
+};
 
 const show = (picture) => {
   const pictureElement = document.querySelector('.big-picture');
   pictureElement.classList.remove('hidden');
   picturePhoto.src = picture.url;
-  pictureLikes.textContent = pictureElement.querySelector('.likes-count');
-  pictureCountComments.textContent = pictureElement.querySelector('.comments-count');
-  pictureDesctiption.textContent = pictureElement.querySelector('.social__caption');
+  pictureLikes.textContent = picture.likes
+  pictureCountComments.textContent = picture.comments.length;
+  pictureDesctiption.textContent = picture.description;
+  renderComments(picture.comments);
   counterCommentBox.classList.add('hidden');
   commentLoader.classList.add('hidden');
-  bodyBackground.classList.add('modal-open');
+  document.body.classList.add('modal-open');
 };
 
 buttonClose.addEventListener('click', function () {
   pictureElement.classList.add('hidden');
 });
-
-const renderComments = (comments) => {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < comments.length; i++) {
-    const commentElement = tamplateFragment.cloneNode(true);
-    commentElement.classList.add('social__comment');
-    fragment.appendChild(commentElement);
-  }
-  pictureBoxComments.appendChild(fragment);
-}
 
 export{show, renderComments};
